9b3140802dffa58c500addd7aa8504908224d7b0
Wilder Moreno <wildev@MacBook-Pro-de-Wilder.local>
2026-04-18 16:17:57 -0500

    chore: Agregar estructura inicial del proyecto

---
+++ App/MaplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaplyApp.swift
+//  Maply
+//
+//  Created by Wilder Moreno on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MaplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Maply
+//
+//  Created by Wilder Moreno on 18/04/26.
+//
+
+import Foundation
